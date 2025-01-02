4e20f856e979d14bfac1b84d41023f5926116286
AvaniSujith <126156788+AvaniSujith@users.noreply.github.com>
2025-01-02 22:51:01 +0530

    New corrections

--- TedTalks/displayNav.js
+++ TedTalks/displayNav.js
@@ -3,4 +3,15 @@ const displayBtn = document.getElementById('search-end-icon');
 displayBtn.addEventListener('click', () =>{
     const div = document.getElementById("show-block")
     div.style.display = (div.style.display === "none" || div.style.display === "") ? "flex" : "none";
+
+    if(div.style.display === "flex"){
+        const style = document.createElement('style');
+        style.textContent = `
+            .search-btns-block{
+                padding-bottom: 1.5rem;
+            } 
+        `;
+        document.head.appendChild(style);
+    }
 })
+
